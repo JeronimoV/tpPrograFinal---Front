d356df77efe8e0e48394a1dcd9539f23eecb1e43
JeronimoV <jeronimoevilar@outlook.com>
2025-11-05 10:06:25 -0300

    fue

--- src/app/components/nav-bar/nav-bar.ts
+++ src/app/components/nav-bar/nav-bar.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
@@ -7,20 +7,5 @@ import { RouterLink } from '@angular/router';
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
-export class NavBar implements OnInit{
-  data : any
-
-  ngOnInit(): void {
-    const local = localStorage.getItem("data")
-    
-    if(local == null){
-      this.data = {userName: "Error", image: "https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_640.png"}
-      console.log("Entre aca");
-      
-    }else{
-      this.data = JSON.parse(local);
-      console.log(this.data.userName);
-      
-    }
-  }
+export class NavBar{
 }
