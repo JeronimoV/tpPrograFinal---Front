f9a938407f79ecf6c94ac9a865fb1dbbae1536c8
JeronimoV <jeronimoevilar@outlook.com>
2025-11-05 08:59:58 -0300

    profile

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { Login } from './pages/login/login';
 import { Register } from './pages/register/register';
 import { Home } from './pages/home/home';
+import { Profile } from './pages/profile/profile';
 
 export const routes: Routes = [
     {path: "login", component: Login},
     {path: "register", component: Register},
+    {path: "profile", component: Profile},
     {path: "", component: Home},
     { path: '**', redirectTo: '' },
 ];
