a8db7c8eb4b5fcdd052bdbde0d612acdee219c65
JeronimoV <jeronimoevilar@outlook.com>
2025-11-12 09:25:55 -0300

    fix cors

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl: "http://localhost:3000/" //'https://tp-progra-final-back.vercel.app/'
+  apiUrl: "https://tp-progra-final-back-1z2yj29nb-jeronimovs-projects.vercel.app/" //"http://localhost:3000/" //'https://tp-progra-final-back.vercel.app/'
 };
