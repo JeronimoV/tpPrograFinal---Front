64b63a86cfc2d0a36bbd09409cd9819a6bd87608
JeronimoV <jeronimoevilar@outlook.com>
2025-11-25 02:12:38 -0300

    test

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  apiUrl:  "https://tp-progra-final-back-1z2yj29nb-jeronimovs-projects.vercel.app/"
+  apiUrl:  "https://tp-progra-final-back.vercel.app/"
 };
